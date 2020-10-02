9387d51956607a126681a7ebc9b8724307436ced
Nic <nf.denuzzo@gmail.com>
2020-10-03 00:36:48 +0100

    update -> logrocket to index not main

--- client/src/store/userProfile.js
+++ client/src/store/userProfile.js
@@ -25,7 +25,6 @@ const myProfile = {
         const result = await axios.axiosInstance.get(`${myProfileUrl}`);
         if (result && result.status === 200) {
           commit("setMyProfile", result.data);
-          console.log("result.data", result.data)
           commit("setMyProfileRetrievedDate", new Date());
           LogRocket.identify(result.data.userEmail, {
             name: result.data.name,
