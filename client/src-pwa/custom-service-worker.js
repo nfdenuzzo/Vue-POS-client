389c79b2270a8922b13fd0c35cfa5feb5e7bd323
Nic <nf.denuzzo@gmail.com>
2020-10-03 22:03:27 +0100

    update

--- client/src-pwa/custom-service-worker.js
+++ client/src-pwa/custom-service-worker.js
@@ -8,50 +8,74 @@
   dependencies
 */
 
-import { precacheAndRoute } from "workbox-precaching";
+import { suppressWarnings, precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
 import { CacheFirst } from "workbox-strategies";
 import { ExpirationPlugin } from "workbox-expiration";
 import { CacheableResponsePlugin } from "workbox-cacheable-response";
 import { NetworkFirst } from "workbox-strategies";
+import { skipWaiting, clientsClaim, setCacheNameDetails } from "workbox-core";
 
 /*
   config
 */
 
-workbox.core.setCacheNameDetails({ prefix: 'd4' })
+setCacheNameDetails({ prefix: "d4" });
 //Change this value every time before you build
-const LATEST_VERSION = 'v1.5'
-self.addEventListener('activate', (event) => {
-  console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
+const LATEST_VERSION = "v1.5";
+self.addEventListener("activate", event => {
+  console.log(`%c ${LATEST_VERSION} `, "background: #ddd; color: #0000ff");
   if (caches) {
-    caches.keys().then((arr) => {
-      arr.forEach((key) => {
-        if (key.indexOf('d4-precache') < -1) {
-          caches.delete(key).then(() => console.log(`%c Cleared ${key}`, 'background: #333; color: #ff0000'))
+    caches.keys().then(arr => {
+      arr.forEach(key => {
+        if (key.indexOf("d4-precache") < -1) {
+          caches
+            .delete(key)
+            .then(() =>
+              console.log(
+                `%c Cleared ${key}`,
+                "background: #333; color: #ff0000"
+              )
+            );
         } else {
-          caches.open(key).then((cache) => {
-            cache.match('version').then((res) => {
+          caches.open(key).then(cache => {
+            cache.match("version").then(res => {
               if (!res) {
-                cache.put('version', new Response(LATEST_VERSION, { status: 200, statusText: LATEST_VERSION }))
+                cache.put(
+                  "version",
+                  new Response(LATEST_VERSION, {
+                    status: 200,
+                    statusText: LATEST_VERSION
+                  })
+                );
               } else if (res.statusText !== LATEST_VERSION) {
-                caches.delete(key).then(() => console.log(`%c Cleared Cache ${LATEST_VERSION}`, 'background: #333; color: #ff0000'))
-              } else console.log(`%c Great you have the latest version ${LATEST_VERSION}`, 'background: #333; color: #00ff00')
-            })
-          })
+                caches
+                  .delete(key)
+                  .then(() =>
+                    console.log(
+                      `%c Cleared Cache ${LATEST_VERSION}`,
+                      "background: #333; color: #ff0000"
+                    )
+                  );
+              } else
+                console.log(
+                  `%c Great you have the latest version ${LATEST_VERSION}`,
+                  "background: #333; color: #00ff00"
+                );
+            });
+          });
         }
-      })
-    })
+      });
+    });
   }
-})
+});
 
-workbox.skipWaiting()
-workbox.clientsClaim()
+clientsClaim();
+skipWaiting();
 
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+suppressWarnings();
+precacheAndRoute(self.__precacheManifest, {});
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -79,10 +103,7 @@ registerRoute(
 
 registerRoute(({ url }) => url.pathname.startsWith("/api"), new NetworkFirst());
 
-registerRoute(
-  ({ url }) => url.href.startsWith("http"),
-  new NetworkFirst()
-);
+registerRoute(({ url }) => url.href.startsWith("http"), new NetworkFirst());
 
 /*
   events - push
