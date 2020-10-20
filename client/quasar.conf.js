a36f2270d91a3254e00864aa0ecca437b2dfa77c
Nic <nf.denuzzo@gmail.com>
2020-10-21 00:10:41 +0100

    update, frontend tweaks

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -53,7 +53,7 @@ module.exports = function (ctx) {
       // publicPath: '',
       vueRouterMode: 'history', // available values: 'hash', 'history'
       env: {
-        VUE_APP_APP_V: "3.8",
+        VUE_APP_APP_V: "0.1",
         NODE_ENV: ctx.dev ? 'development' : 'production',
         // we used the commented out code during development but seems no longer needed with the DNS settings
         BASE_URL: ctx.dev ? '/' : 'https://trusting-kilby-b32d22.netlify.app/',
